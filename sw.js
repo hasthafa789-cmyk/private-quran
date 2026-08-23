2e05236cf3db0057e0960a9b507934504e1c4112
hasthafa789-cmyk <hasthafa789@gmail.com>
2026-08-23 07:32:06 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker untuk PWA
+const CACHE_NAME = 'hasnan-app-v1';
+
+self.addEventListener('install', event => {
+    console.log('[ServiceWorker] Install');
+    // Memaksa browser segera memakai Service Worker baru jika ada update
+    self.skipWaiting(); 
+});
+
+self.addEventListener('activate', event => {
+    console.log('[ServiceWorker] Activate');
+});
+
+// Menangkap sinyal internet untuk mode offline dasar
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
+});
